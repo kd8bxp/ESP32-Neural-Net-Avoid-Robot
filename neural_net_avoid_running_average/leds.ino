90949da357a93961780eca289bff3981d6ef1d3d
KD8BXP <kd8bxp@aol.com>
2022-06-01 08:07:17 -0400

    mixed training data minor work on leds

--- neural_net_avoid_running_average/leds.ino
+++ neural_net_avoid_running_average/leds.ino
@@ -1,8 +1,8 @@
 void set_leds()  // input activation
 {   
   led_clear();  
-  leds[20] = CHSV((cells_in[0]*150),(cells_in[0]*80),(cells_in[0]*15)); /*pixels.setPixelColor(2, pixels.Color(cells_in[0]*3, 0,0));*/ FastLED.show();//delay(2); 
-  leds[15] = CHSV((cells_in[1]*150),(cells_in[1]*15),(cells_in[1]*80)); /*pixels.setPixelColor(5, pixels.Color(cells_in[1]*3, 0,0));*/ FastLED.show();//delay(2); 
+  leds[20] = CHSV(result1.getAverage(),80,15); /*pixels.setPixelColor(2, pixels.Color(cells_in[0]*3, 0,0));*/ FastLED.show();//delay(2); 
+  leds[15] = CHSV(result2.getAverage(),15,80); /*pixels.setPixelColor(5, pixels.Color(cells_in[1]*3, 0,0));*/ FastLED.show();//delay(2); 
   leds[out+1] = CHSV(150,180,150); /*pixels.setPixelColor(out+18,150, 0, 0);*/ FastLED.show(); //delay(2); 
  //FastLED.show();
 }
